1d84cfaa6cdedd40c6e75237994500183db191d0
lssromel <lssromel@hotmail.com>
2017-06-15 15:09:20 -0500

    Create settings.py

--- result_app/settings.py
+++ result_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '68o6)^rv3$py&u3k(gsmwymvu9wiin%st6x=q$3#trgjnxjpi='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["34.205.92.167"]
 
